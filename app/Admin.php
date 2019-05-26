b0124c1abadb0b2bad177b184953e7136a2eeb27
Huy Pham <phamhuyqn92@gmail.com>
2019-05-26 11:20:21 +0700

    a

--- app/Admin.php
+++ app/Admin.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+//hello
 class Admin extends Authenticatable
 {
     use Notifiable;
